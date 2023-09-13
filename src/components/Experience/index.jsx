bcfe3b87708e32e4b3a879023fdb0c2f46abfabf
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2023-09-13 22:48:33 +0530

    update portfolio

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -77,8 +77,7 @@ const index = () => {
       <Wrapper>
         <Title>Experience</Title>
         <Desc>
-          My work experience as a software engineer and working on different
-          companies and projects.
+          My work experience as a Full Stack Web Developer and working on different projects.
         </Desc>
         <TimelineSection>
           <Timeline>
